e47ee644490735eebc76b7ff74a2682be4323df2
cxh <2604856589@qq.com>
2023-03-29 19:30:02 +0800

    test: 测试架构搭建

---
+++ examples/vanilla/main.ts
@@ -0,0 +1,25 @@
+// import './assets/style.css'
+// import javascriptLogo from './assets/javascript.svg'
+// import viteLogo from './assets/vite.svg'
+import { add } from '@web-tracing/core'
+
+console.log('add', add)
+console.log(11)
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
