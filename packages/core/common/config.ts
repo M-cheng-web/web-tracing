c302c149d530c139c6822615ee571940c935e202
cxh <2604856589@qq.com>
2023-04-06 16:25:01 +0800

    style: config

--- packages/core/common/config.ts
+++ packages/core/common/config.ts
@@ -1,4 +1,17 @@
-import { version } from '../package.json'
+import { name, version } from '../package.json'
 
-export const SDK_NAME = 'web-tracing';
 export const SDK_VERSION = version;
+
+export const SDK_NAME = name;
+
+export const DEVICE_KEY = '_webtracing_device_id'; // 设备ID Key - 私有属性
+
+export const SESSION_KEY = '_webtracing_session_id'; // 会话ID Key(一个站点只允许运行一个埋点程序) - 私有属性
+
+export const SURVIVIE_MILLI_SECONDS = 1800000; // 会话 session存活时长(30minutes) - 私有属性
+
+export const MAX_CACHE_LEN = 5; // 最大缓存数
+
+export const MAX_WAITING_TIME = 5000; // 最大等待时间
+
+export const DEBUG_LOG = true; // 是否输出采集信息
--- packages/core/utils/session.ts
+++ packages/core/utils/session.ts
@@ -2,7 +2,7 @@
  * 会话控制,此会话只和具体的浏览器相关,与业务无关,和业务意义上的登录态没有任何关联,只是用于追踪同一个浏览器上访问页面的动作
  */
 import { getCookieByName, uuid } from './index';
-import { SURVIVIE_MILLI_SECONDS, SESSION_KEY } from '../common/constant'
+import { SURVIVIE_MILLI_SECONDS, SESSION_KEY } from '../common'
 
 /**
  * 刷新会话存续期
