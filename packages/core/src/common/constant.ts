d67fdff00b30fecd7f1681978ba0141cc9b9b92c
cxh <2604856589@qq.com>
2023-04-14 16:20:49 +0800

    style

--- packages/core/src/common/constant.ts
+++ packages/core/src/common/constant.ts
@@ -1,4 +1,23 @@
 /**
+ * 事件类型
+ */
+export enum EVENTTYPES {
+  ERROR = 'error',
+  CONSOLEERROR = 'consoleError',
+  UNHANDLEDREJECTION = 'unhandledrejection',
+  CLICK = 'click',
+  LOAD = 'load',
+  BEFOREUNLOAD = 'beforeunload',
+  FETCH = 'fetch',
+  XHROPEN = 'xhr-open',
+  XHRSEND = 'xhr-send',
+  HASHCHANGE = 'hashchange',
+  HISTORYPUSHSTATE = 'history-pushState',
+  HISTORYREPLACESTATE = 'history-replaceState',
+  POPSTATE = 'popstate'
+}
+
+/**
  * 接口错误状态
  */
 export enum SpanStatus {
@@ -39,25 +58,6 @@ export enum BREADCRUMBTYPES {
 export enum STATUS_CODE {
   ERROR = 'error',
   OK = 'ok'
-}
-
-/**
- * 事件类型
- */
-export enum EVENTTYPES {
-  ERROR = 'error',
-  CONSOLEERROR = 'consoleError',
-  UNHANDLEDREJECTION = 'unhandledrejection',
-  CLICK = 'click',
-  LOAD = 'load',
-  BEFOREUNLOAD = 'beforeunload',
-  FETCH = 'fetch',
-  XHROPEN = 'xhr-open',
-  XHRSEND = 'xhr-send',
-  HASHCHANGE = 'hashchange',
-  HISTORYPUSHSTATE = 'history-pushState',
-  HISTORYREPLACESTATE = 'history-replaceState',
-  POPSTATE = 'popstate'
 }
 
 export enum HTTPTYPE {
