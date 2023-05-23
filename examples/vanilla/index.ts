fb37fe31e59456c13bb00e268d23325090c27f33
cxh <2604856589@qq.com>
2023-05-23 10:08:45 +0800

    test: 测试sourcemap

---
+++ examples/vanilla/index.ts
@@ -0,0 +1,24 @@
+// ---------------- Error 捕捉 ----------------
+document.getElementById('codeErr')?.addEventListener('click', () => {
+  console.log(222)
+  codeError()
+})
+
+function codeError() {
+  const a = {}
+  a.split('/')
+}
+function promiseError() {
+  const promiseWrap = () =>
+    new Promise((resolve, reject) => {
+      reject('promise reject')
+    })
+  promiseWrap().then(res => {
+    console.log('res', res)
+  })
+}
+function consoleErr() {
+  console.error('consoleErr1', 'consoleErr1.1', 'consoleErr1.2')
+  // console.error(111);
+  // console.error(new Error("谢谢谢谢谢"));
+}
