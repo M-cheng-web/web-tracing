f5130f1e097da118c88b7b54fcd21e55e126c489
cxh <2604856589@qq.com>
2023-06-01 20:37:21 +0800

    style

--- packages/core/src/lib/replace.ts
+++ packages/core/src/lib/replace.ts
@@ -1,5 +1,4 @@
 import type { VoidFun } from '../types'
-import { debug } from '../utils/debug'
 import { _global } from '../utils/global'
 import { on, replaceAop, throttle, isValidKey } from '../utils'
 import { EVENTTYPES } from '../common'
@@ -7,61 +6,16 @@ import { eventBus } from './eventBus'
 
 /**
  * 根据入参初始化 重写、监听
+ *
+ * 定义一个产出模块
+ * 其注册所有模块所需要的 - 监听，改写
+ * 当那些模块要用的时候，去产出模块那数据，这个模块的数据不应该有业务数据
+ * 只负责去采集数据，具体怎么拿是业务模块需要去分辨的，采集哪些数据是注册的时候决定的
+ * 只会管兼容性问题，不兼容的api就不做动作
+ * 这样就不需要所有的模块都要init了，之后需要init的模块，可以专门去做模块内的事
+ * 注意这里面耦合性不能太高，不要抽了还不如不抽，做到模块内改了代码不需要去另外个文件再改
  */
 export function initReplace(): void {
-  const baseReplace: EVENTTYPES[] = []
-  const allReplace: EVENTTYPES[] = [...baseReplace]
-
-  // 定义一个产出模块
-  // 其注册所有模块所需要的 - 监听，改写
-  // 当那些模块要用的时候，去产出模块那数据
-  // 这个模块的数据不应该有业务数据
-  // 只负责去采集数据，具体怎么拿是业务模块需要去分辨的
-  // 采集哪些数据是注册的时候决定的
-  // 只会管兼容性问题，不兼容的api就不做动作
-
-  // 这样就不需要所有的模块都要init了
-  // 之后需要init的模块，可以专门去做模块内的事
-
-  // 注意这里面耦合性不能太高，不要抽了还不如不抽
-  // 做到模块内改了代码不需要去另外个文件再改
-  // 现在要解决的是，setupReplace初始化的时候，怎么知道要加载哪些监听
-
-  // 这个后面改成键值对的方式会好些把，但要考虑有些或的逻辑
-
-  // 这里先不用这种方式，采用全局挂载
-  // if (options.error.core) {
-  //   allReplace.push(EVENTTYPES.ERROR) // 监听捕获错误
-  //   allReplace.push(EVENTTYPES.UNHANDLEDREJECTION) // 监听handleUnhandleRejection事件
-  //   allReplace.push(EVENTTYPES.CONSOLEERROR) // 重写console.error
-  // }
-
-  // if (options.event.core) {
-  //   allReplace.push(EVENTTYPES.CLICK) // 监听click事件
-  // }
-
-  // allReplace.push(EVENTTYPES.BEFOREUNLOAD) // 监听beforeunload事件
-
-  // if (options.performance.server) {
-  //   allReplace.push(EVENTTYPES.XHROPEN) // 重写XMLHttpRequest-open
-  //   allReplace.push(EVENTTYPES.XHRSEND) // 重写XMLHttpRequest-send
-  //   allReplace.push(EVENTTYPES.FETCH) // 重写fetch
-  // }
-
-  // if (options.performance.firstResource) {
-  //   allReplace.push(EVENTTYPES.LOAD) // 监听load事件
-  // }
-
-  // if (options.pv.core) {
-  //   allReplace.push(EVENTTYPES.HISTORYPUSHSTATE)
-  //   allReplace.push(EVENTTYPES.HISTORYREPLACESTATE)
-
-  //   if (options.pv.hashtag) {
-  //     allReplace.push(EVENTTYPES.HASHCHANGE) // 监听hashchange
-  //     allReplace.push(EVENTTYPES.POPSTATE)
-  //   }
-  // }
-
   for (const key in EVENTTYPES) {
     if (isValidKey(key, EVENTTYPES)) {
       replace(key)
