eff6933fbac1c6b260e84a6976b8a8b4ece092fb
cxh <2604856589@qq.com>
2023-06-16 16:33:06 +0800

    feat: vue2的错误拦截优化

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -4,7 +4,7 @@ import { initOptions } from './src/lib/options'
 import { initBase } from './src/lib/base'
 import { initSendData } from './src/lib/sendData'
 import { initLineStatus } from './src/lib/line-status'
-import { initError } from './src/lib/err'
+import { initError, parseError } from './src/lib/err'
 import { initEvent } from './src/lib/event'
 import { initEventDwell } from './src/lib/event-dwell'
 import { initHttp } from './src/lib/http'
@@ -12,6 +12,7 @@ import { initPerformance } from './src/lib/performance'
 import { initPv } from './src/lib/pv'
 import { initIntersection } from './src/lib/intersectionObserver'
 import { _global } from './src/utils/global'
+import { SENDID } from './src/common'
 import { logError } from './src/utils/debug'
 import { initRecordScreen } from './src/lib/recordscreen'
 import * as exportMethods from './src/lib/exportMethods'
@@ -40,6 +41,6 @@ function init(options: InitOptions): void {
   _global.__webTracingInit__ = true
 }
 
-export { init, InitOptions, logError }
+export { init, InitOptions, logError, parseError, SENDID }
 export * from './src/lib/exportMethods'
 export default { init, ...exportMethods }
--- packages/vue2/index.ts
+++ packages/vue2/index.ts
@@ -1,21 +1,29 @@
-import { init, InitOptions, traceError, logError } from '@web-tracing/core'
+import {
+  init,
+  InitOptions,
+  traceError,
+  logError,
+  parseError,
+  SENDID
+} from '@web-tracing/core'
 
 function install(Vue: any, options: InitOptions) {
   const handler = Vue.config.errorHandler
   Vue.config.errorHandler = function (err: Error, vm: any, info: string): void {
-    const match = err.stack!.match(/(?<=http:\/\/.*:\d+\/).*:\d+:\d+/)
-
-    const position = match ? match[0] : ''
-    const line = position.split(':')[1] // 行
-    const col = position.split(':')[2] // 列
+    // const match = err.stack!.match(/(?<=http:\/\/.*:\d+\/).*:\d+:\d+/)
+    // const position = match ? match[0] : ''
+    // const line = position.split(':')[1] // 行
+    // const col = position.split(':')[2] // 列
+    // traceError({
+    //   eventId: err.name,
+    //   errMessage: err.message,
+    //   line,
+    //   col
+    // })
 
     logError(err)
-    traceError({
-      eventId: err.name,
-      errMessage: err.message,
-      line,
-      col
-    })
+    const errorInfo = { eventId: SENDID.CODE, ...parseError(err) }
+    traceError(errorInfo)
     if (handler) handler.apply(null, [err, vm, info])
   }
   init(options)
