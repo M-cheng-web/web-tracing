52a57e99d51f0f90e0fe0418bf00f39e0fce073e
cxh <2604856589@qq.com>
2023-06-21 14:37:18 +0800

    feat: 添加公共utils

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -475,3 +475,33 @@ export function parseGetParams(url: string): AnyObj<string> {
   }
   return params
 }
+
+/**
+ * 深拷贝
+ * 兼容函数，对象，相互引用场景
+ * @param target 需要深拷贝的原对象
+ * @return 深拷贝后的对象
+ */
+export function deepCopy<T>(target: T, map = new Map()) {
+  if (target !== null && typeof target === 'object') {
+    let res = map.get(target)
+    if (res) return res
+    if (target instanceof Array) {
+      res = []
+      map.set(target, res)
+      target.forEach((item, index) => {
+        res[index] = deepCopy(item, map)
+      })
+    } else {
+      res = {}
+      map.set(target, res)
+      Object.keys(target).forEach(key => {
+        if (isValidKey(key, target)) {
+          res[key] = deepCopy(target[key], map)
+        }
+      })
+    }
+    return res
+  }
+  return target
+}
