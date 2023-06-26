f64c2a8e19d94a2e655dd86b3b79b4e745f68eee
cxh <2604856589@qq.com>
2023-06-26 23:59:35 +0800

    feat(vue3): 导出core所有导出的

--- packages/vue3/index.ts
+++ packages/vue3/index.ts
@@ -1,20 +1,8 @@
-import {
-  init,
-  InitOptions,
-  traceError,
-  logError,
-  parseError,
-  SENDID
-} from '@web-tracing/core'
+import { init, InitOptions } from '@web-tracing/core'
 
 function install(app: any, options: InitOptions) {
-  // app.config.errorHandler = function (err: Error): void {
-  //   console.log('err', err)
-  //   logError(err)
-  //   const errorInfo = { eventId: SENDID.CODE, ...parseError(err) }
-  //   traceError(errorInfo)
-  // }
   init(options)
 }
 
 export default { install }
+export * from '@web-tracing/core'
