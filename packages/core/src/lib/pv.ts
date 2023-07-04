b1c00dbad392ebeca6a480ea49be61857b92c7ab
cxh <2604856589@qq.com>
2023-07-04 11:24:44 +0800

    feat: pv模块完善部分缺陷以及添加更多说明

--- packages/core/src/lib/pv.ts
+++ packages/core/src/lib/pv.ts
@@ -5,30 +5,46 @@ import { getLocationHref, getTimestamp } from '../utils'
 import { _global } from '../utils/global'
 import { options } from './options'
 import { eventBus } from './eventBus'
-import { EVENTTYPES, SEDNEVENTTYPES } from '../common'
+import { EVENTTYPES, SEDNEVENTTYPES, WEBPAGELOAD } from '../common'
 
 let oldURL = getLocationHref()
-let historyLength = _global.history.length
 
 /**
  * 路由Pv采集
  *
  * 分为以下几种情况（sdk是兼容vue2以及vue3的，但可能有些部分是取了它们的一些特性的，可能会受到后续它们的更改的影响）
+ * 针对 history/hash 以及 刷新页面/初始加载 的情况获取到的数据进行说明
  *
  * 1. 针对普通 html
- *    1. window.history.pushState 时会触发 pushState
- *    2. window.history.replaceState 时其会触发 replaceState
- *    3. 手动 更改地址栏地址 时其触发顺序： popstate -> hashchange (history 模式手动更改地址会当做刷新页面，什么也不会触发)
+ *    1. 初始化项目：不触发这里的钩子，只会触发首次进入页面方法(referer=''、action=navigation)
+ *    2. 手动刷新当前页：不触发这里的钩子，只会触发首次进入页面方法(referer=''、action=reload)
+ *    3. window.history.pushState 时会触发 pushState
+ *    4. window.history.replaceState 时其会触发 replaceState
+ *    5. 手动 更改地址栏地址(或者window.location.href) 时其触发顺序： popstate -> hashchange (history 模式手动更改地址会视为初始化项目)
  *
  * 2. 针对 vue2(vue-router)
- *    1. 手动 push 时其会触发 pushState (history、hash模式表现相同)
- *    2. 手动 repleace 时其会触发 replaceState (history、hash模式表现相同)
- *    3. 手动 更改地址栏地址 时其触发顺序： popstate -> hashchange (history 模式手动更改地址会当做刷新页面，什么也不会触发)
+ *    1. 手动刷新当前页
+ *       1. history模式：不触发这里的钩子，只会触发首次进入页面方法(referer=''、action=reload)
+ *       2. hash模式：触发首次进入页面的方法(referer=''、action=reload) + replaceState 钩子(referer字段与triggerPageUrl字段相同)
+ *    2. 初始化项目
+ *       1. history模式：不触发这里的钩子，只会触发首次进入页面方法(referer=''、action=navigation)
+ *       2. hash模式：触发首次进入页面的方法(action=reload；referer字段为网址根路径) + replaceState 钩子(action=navigation；referer字段与triggerPageUrl字段相同)
+ *    3. push 时其会触发 pushState (history、hash模式表现相同)
+ *    4. repleace 时其会触发 replaceState (history、hash模式表现相同)
+ *    5. 更改地址栏地址 时其触发顺序： popstate -> hashchange (history 模式手动更改地址会当做刷新页面，什么也不会触发)
+ *    6. 浏览器回退：popstate -> hashchange (history模式只触发 popstate)
  *
  * 3. 针对 vue3(vue-router)
- *    1. 手动 push 时其触发顺序： replaceState -> pushState (history、hash模式表现相同)
- *    2. 手动 repleace 时其会触发 replaceState (history、hash模式表现相同)
- *    3. 手动 更改地址栏地址 时其触发顺序： replaceState -> popstate -> hashchange (history 模式仅触发 replaceState)
+ *    1. 手动刷新当前页
+ *       1. history模式：触发首次进入页面的方法(action=reload；referer字段为空) + replaceState 钩子(action=navigation；referer字段与triggerPageUrl字段相同)
+ *       2. hash模式：触发首次进入页面的方法(action=reload；referer字段为空) + replaceState 钩子(action=navigation；referer字段与triggerPageUrl字段相同)
+ *    2. 初始化项目
+ *       1. history模式：触发首次进入页面的方法(action=navigation；referer字段为空) + replaceState 钩子(action=navigation；referer字段与triggerPageUrl字段相同)
+ *       2. hash模式：触发首次进入页面的方法(action=navigation；referer字段为空) + replaceState 钩子(action=navigation；referer字段与triggerPageUrl字段相同)
+ *    3. push 时其触发顺序： replaceState -> pushState (history、hash模式表现相同)
+ *    4. repleace 时其会触发 replaceState (history、hash模式表现相同)
+ *    5. 更改地址栏地址 时其触发顺序： replaceState -> popstate -> hashchange (history 模式仅触发 replaceState)
+ *    6. 浏览器回退：popstate -> hashchange (history模式只触发 popstate)
  */
 function initPv() {
   if (!options.value.pv.core) return
@@ -36,23 +52,25 @@ function initPv() {
   let lastIsPop = false // 最后一次触发路由变化是否为popState触发
   let repetitionRoute = false // 在触发 replaceState 后 100ms 内的 pushState 会被无效记录
 
-  sendPageView({ url: oldURL, referer: document.referrer }) // 首次进入记录url变化
+  sendPageView({ referer: document.referrer }) // 首次进入记录url变化
 
   eventBus.addEvent({
     type: EVENTTYPES.HISTORYPUSHSTATE,
     callback: () => {
+      console.log('pushState')
       if (repetitionRoute) return
       lastIsPop = false
-      sendPageView({ actions: 'navigation', referer: getLocationHref() })
+      sendPageView({ action: 'navigation' })
     }
   })
 
   eventBus.addEvent({
     type: EVENTTYPES.HISTORYREPLACESTATE,
     callback: () => {
+      console.log('replaceState')
       repetitionRoute = true
       lastIsPop = false
-      sendPageView({ actions: 'navigation', referer: getLocationHref() })
+      sendPageView({ action: 'navigation' })
       setTimeout(() => {
         repetitionRoute = false
       }, 100)
@@ -62,18 +80,17 @@ function initPv() {
   eventBus.addEvent({
     type: EVENTTYPES.HASHCHANGE,
     callback: () => {
+      console.log('hashchange')
       if (repetitionRoute) return
-      if (options.value.pv.hashtag && !lastIsPop) sendPageView()
+      if (!lastIsPop) sendPageView()
       lastIsPop = false
     }
   })
 
-  // hash变化也会触发 popstate 事件,而且会先触发popstate事件
-  // 可以使用popstate来代替hashchange(需要支持History H5 Api)
-  // https://developer.mozilla.org/zh-CN/docs/Web/API/Window/popstate_event
   eventBus.addEvent({
     type: EVENTTYPES.POPSTATE,
     callback: () => {
+      console.log('popstate')
       if (repetitionRoute) return
       if (_global.location.hash !== '') {
         const oldHost =
@@ -82,8 +99,7 @@ function initPv() {
             : oldURL
         if (
           _global.location.href.slice(0, _global.location.href.indexOf('#')) ===
-            oldHost &&
-          !options.value.pv.hashtag
+          oldHost
         )
           return
       }
@@ -97,33 +113,31 @@ function initPv() {
  * 发送数据
  */
 function sendPageView(option: AnyObj = {}) {
-  const { url, referer = oldURL, actions = '', params } = option
-  let action = actions
-  if (!action && _global.history.length < 50) {
-    action =
-      historyLength === _global.history.length ? 'back_forward' : 'navigation'
-    historyLength = _global.history.length
+  const { referer = oldURL, action, params, title } = option
+  let _action = action
+  if (!_action) {
+    _action = WEBPAGELOAD[performance.navigation.type] || ''
   }
 
   // 如果option.title为空,则等待框架处理document.title,延迟17ms
   // 为什么是17ms?  一秒60Hz是基准,平均1Hz是17毫秒,只要出来了页面那就有 document.title
   setTimeout(
     () => {
+      oldURL = getLocationHref()
+
       sendData.emit({
         eventType: SEDNEVENTTYPES.PV,
         eventId: baseInfo.pageId,
-        triggerPageUrl: url || getLocationHref(),
+        triggerPageUrl: getLocationHref(),
         referer,
         params,
-        title: option.title || document.title,
-        action,
+        title: title || document.title,
+        action: _action,
         triggerTime: getTimestamp()
       })
     },
-    option.title ? 0 : 17
+    title ? 0 : 17
   )
-  oldURL = url || getLocationHref()
-  historyLength = _global.history.length
 }
 
 /**
@@ -134,13 +148,13 @@ function handleSendPageView(options: AnyObj = {}) {
   setTimeout(
     () => {
       sendData.emit({
+        referer: oldURL,
+        title: document.title,
         ...options,
         eventType: SEDNEVENTTYPES.PV,
         eventId: baseInfo.pageId,
         triggerPageUrl: getLocationHref(),
-        triggerTime: getTimestamp(),
-        title: options.title || document.title,
-        referer: oldURL
+        triggerTime: getTimestamp()
       })
     },
     options.title ? 0 : 17
