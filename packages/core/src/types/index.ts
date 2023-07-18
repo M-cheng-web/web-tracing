90f3a219a92678e7f6d1097374a8fe01e9227af3
cxh <2604856589@qq.com>
2023-07-18 16:10:21 +0800

    style: ts

--- packages/core/src/types/index.ts
+++ packages/core/src/types/index.ts
@@ -40,10 +40,10 @@ export type InternalOptions = {
   userUuid: string // 用户id(外部填充进来的id)
   sdkUserUuid: string // 用户id(sdk内部生成的id)
   debug: boolean // 是否开启调试模式(控制台会输出sdk动作)
-  pv: Pv | boolean
-  performance: Performance | boolean
-  error: Error | boolean
-  event: Event | boolean
+  pv: Pv
+  performance: Performance
+  error: Error
+  event: Event
   ext: AnyObj // 自定义全局附加参数(放在baseInfo中)
   tracesSampleRate: number // 抽样发送
   cacheMaxLength: number // 上报数据最大缓存数
@@ -69,10 +69,10 @@ export type InitOptions = {
   appVersion?: string // 应用版本
   userUuid?: string // 用户id(外部填充进来的id)
   debug?: boolean // 是否开启调试模式(控制台会输出sdk动作)
-  pv?: Pv
-  performance?: Performance
-  error?: Error
-  event?: Event
+  pv?: Pv | boolean
+  performance?: Performance | boolean
+  error?: Error | boolean
+  event?: Event | boolean
   ext?: { [key: string]: any } // 自定义全局附加参数(放在baseInfo中)
   tracesSampleRate?: number // 抽样发送
   cacheMaxLength?: number // 上报数据最大缓存数
