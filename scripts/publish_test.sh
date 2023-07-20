f0c0ddd8205e0952fc01f5d30ccffa18c51d858d
cxh <2604856589@qq.com>
2023-07-20 21:08:38 +0800

    chore

---
+++ scripts/publish_test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# 发布文档
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+pnpm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vitepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:M-cheng-web/web-tracing.git main:gh-pages
+
+cd -
