beb52645d9894744113035d16d47f4bd0a9f5c30
SaberAlter <zzqchongya@foxmail.com>
2023-12-24 23:37:21 +0800

    feat: 添加 electron 支持

--- packages/core/src/utils/global.ts
+++ packages/core/src/utils/global.ts
@@ -7,10 +7,15 @@ import { WebTracing } from '../types'
 export const isBrowserEnv = isWindow(typeof window !== 'undefined' ? window : 0)
 
 /**
+ * 是否为 electron 环境
+ */
+export const isElectronEnv = !!window?.process?.versions?.electron
+
+/**
  * 获取全局变量
  */
 export function getGlobal(): Window {
-  if (isBrowserEnv) return window
+  if (isBrowserEnv || isElectronEnv) return window
   return {} as Window
 }
 
